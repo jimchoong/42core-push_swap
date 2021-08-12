f754fed3717a73a02892e4cf7081d37b7937c790
Jimmy Choong <jimchoong87+github@gmail.com>
2021-08-12 11:53:28 +0800

    first commit

---
+++ push_swap_main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "ps.h"
+
+int	verify_input()
+{
+	if arg
+}
+
+int	main(int argc, char *argv[])
+{
+	if (argc <= 1)
+		return (0);
+	if (argc >= 2)
+	{
+		if(verify_input(argc, argv))
+		{
+			
+		}
+		else
+			ft_putstr_fd("Error", 1);
+	}
+
+	return (0);
+}
